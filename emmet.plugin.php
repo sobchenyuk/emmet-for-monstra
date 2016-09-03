0caf4f02f1b094883b603adbdabb0384ca88af02
sobchenyuk <ablehands@ukr.net>
2016-09-04 00:28:37 +0300

    Add files via upload

--- emmet.plugin.php
+++ emmet.plugin.php
@@ -43,21 +43,27 @@ class Emmet
     public static function EmmetScript()
     {
 
-        if (Request::get('action') == 'add_styles') {
+        if (Request::get('action') == 'add_styles'|| Request::get('action') == 'edit_styles') {
             echo '<script>
-
-$(document).ready(function() {
-        $("textarea#content").addClass("emmet-syntax-css lined").css("display","block");
-        $(".CodeMirror.cm-s-mdn-like").css("display","none");
+         
+         $(document).ready(function() {
+        $("textarea#content").addClass("emmet-syntax-css lined");
         $(".source-editor.emmet-syntax-css.lined").linedtextarea(
 		{selectedLine: 1}
 	    );
+        
+
+
+        editor.setCursor(2,2);     // это значит поместить курсор на 3 строку (отсчёт от 0), символ 3
+
+var pos=editor.posFromIndex(3);  //получить координаты 3-ей позиции (строку и символ)
+editor.setCursor(pos.line,pos.ch);
          });
          
-         require([\'stylesheet\', \'events\'], function(stylesheet, events){
+          require([\'stylesheet\', \'events\'], function(stylesheet, events){
 	var _timedPub,
 		indentation = \'    \',
-		
+		$editor = $(\'.CodeMirror.cm-s-mdn-like\').val(),
 		style = document.createElement(\'style\');
 	document.body.appendChild(style);
 	stylesheet.init({
@@ -68,26 +74,9 @@ $(document).ready(function() {
     pretty_break: true,
     use_tab: true
 	});
-	var texthandler = function(e){
-		if(_timedPub !== false){
-			window.clearTimeout(_timedPub);
-		}
-		_timedPub = window.setTimeout(function(){
-			events.publish(\'/push/rules\', $editor.val());
-      _timedPub = false;
-		}, 500);
-	};
-	events.subscribe(\'/push/rules\', function(val){
-		stylesheet.setString(val);
-	});
-	$editor.on(\'keydown\', texthandler);
-	/* Only here to show that the indentation rule is flexible */
-  events.publish(\'/push/rules\', $editor.val());
-	$(\'input[name=\"indent\"]\').val(indentation),
-	$(\'button[name=\"update\"]\').on(\'click\', function(){
-		indentation = $(\'input[name=\"indent\"]\').val().replace(\'\\t\',\'\t\');
-	});
 });
+
+
 </script>';
         }else{
             echo("
